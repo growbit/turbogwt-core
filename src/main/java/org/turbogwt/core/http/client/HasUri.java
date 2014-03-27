7d1cd2e7be1ddb09f05aa2658efe93f6ccb2a635
Danilo Reinert <daniloreinert@gmail.com>
2014-03-26 22:16:24 -0300

    Abstracted uri parts to a type that generically handles uri creation.

--- src/main/java/org/turbogwt/core/http/client/HasUri.java
+++ src/main/java/org/turbogwt/core/http/client/HasUri.java
@@ -17,73 +17,44 @@
 package org.turbogwt.core.http.client;
 
 /**
- * Utility class for building URIs from their components.
- * <p/>
- * It is NOT aware of templates.
- * <p/>
- * <p>Builder methods perform contextual encoding of characters not permitted in the corresponding URI component
- * following the rules of the
- * <a href="http://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1">application/x-www-form-urlencoded</a>
- * media type for query parameters and <a href="http://ietf.org/rfc/rfc3986.txt">RFC 3986</a> for all other components.
- * Note that only characters not permitted in a particular component are subject to encoding so, e.g., a path supplied
- * to one of the {@code path} methods may contain matrix parameters or multiple path segments since the separators are
- * legal characters and will not be encoded. Percent encoded values are also recognized where allowed and will not be
- * double encoded.</p>
+ * A type that supports uri progressive construction.
+ *
+ * @author Danilo Reinert
  */
-public interface UriBuilder extends HasUri {
-
-    /**
-     * Set the strategy for appending parameters with multiple values.
-     *
-     * @param strategy the strategy.
-     *
-     * @return the updated UriBuilder
-     *
-     * @throws IllegalArgumentException if strategy is null
-     */
-    UriBuilder multipleParamStrategy(MultipleParamStrategy strategy) throws IllegalArgumentException;
-
-    /**
-     * Set the URI user-info.
-     *
-     * @param ui the URI user-info. A null value will unset userInfo component of the URI.
-     *
-     * @return the updated UriBuilder
-     */
-    UriBuilder userInfo(String ui);
+public interface HasUri {
 
     /**
      * Set the URI scheme.
      *
      * @param scheme the URI scheme. A null value will unset the URI scheme.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if scheme is invalid
      */
-    UriBuilder scheme(String scheme) throws IllegalArgumentException;
+    HasUri scheme(String scheme) throws IllegalArgumentException;
 
     /**
      * Set the URI host.
      *
      * @param host the URI host. A null value will unset the host component of the URI.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if host is invalid.
      */
-    UriBuilder host(String host) throws IllegalArgumentException;
+    HasUri host(String host) throws IllegalArgumentException;
 
     /**
      * Set the URI port.
      *
      * @param port the URI port, a negative value will unset an explicit port.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if port is invalid
      */
-    UriBuilder port(int port) throws IllegalArgumentException;
+    HasUri port(int port) throws IllegalArgumentException;
 
     /**
      * Set the URI path. This method will overwrite any existing path and associated matrix parameters. Existing '/'
@@ -91,9 +62,9 @@ public interface UriBuilder extends HasUri {
      *
      * @param path the path. A null value will unset the path component of the URI.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      */
-    UriBuilder path(String path);
+    HasUri path(String path);
 
     /**
      * Append path segments to the existing path. When constructing the final path, a '/' separator will be inserted
@@ -103,11 +74,11 @@ public interface UriBuilder extends HasUri {
      *
      * @param segments the path segment values
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if segments or any element of segments is null
      */
-    UriBuilder segment(String... segments) throws IllegalArgumentException;
+    HasUri segment(String... segments) throws IllegalArgumentException;
 
     /**
      * Append a matrix parameter to the existing set of matrix parameters of the current final segment of the URI path.
@@ -119,12 +90,12 @@ public interface UriBuilder extends HasUri {
      * @param values the matrix parameter value(s), each object will be converted to a {@code String} using its {@code
      *               toString()} method.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if name or values is null
      * @see <a href="http://www.w3.org/DesignIssues/MatrixURIs.html">Matrix URIs</a>
      */
-    UriBuilder matrixParam(String name, Object... values) throws IllegalArgumentException;
+    HasUri matrixParam(String name, Object... values) throws IllegalArgumentException;
 
     /**
      * Append a query parameter to the existing set of query parameters. If multiple values are supplied the parameter
@@ -134,25 +105,18 @@ public interface UriBuilder extends HasUri {
      * @param values the query parameter value(s), each object will be converted to a {@code String} using its {@code
      *               toString()} method.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      *
      * @throws IllegalArgumentException if name or values is null
      */
-    UriBuilder queryParam(String name, Object... values) throws IllegalArgumentException;
+    HasUri queryParam(String name, Object... values) throws IllegalArgumentException;
 
     /**
      * Set the URI fragment.
      *
      * @param fragment the URI fragment. A null value will remove any existing fragment.
      *
-     * @return the updated UriBuilder
+     * @return the updated HasUri
      */
-    UriBuilder fragment(String fragment);
-
-    /**
-     * Build a URI.
-     *
-     * @return the URI built from the UriBuilder as String
-     */
-    String build();
+    HasUri fragment(String fragment);
 }
