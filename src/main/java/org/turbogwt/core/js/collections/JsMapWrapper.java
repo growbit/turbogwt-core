1a130a05abcad73032e49c4cd7913d49d66dd9bf
Danilo Reinert <daniloreinert@gmail.com>
2014-05-21 11:30:08 -0300

    JsMapWrapper improvements.

--- src/main/java/org/turbogwt/core/js/collections/JsMapWrapper.java
+++ src/main/java/org/turbogwt/core/js/collections/JsMapWrapper.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2009 Thomas Broyer
+ * Copyright 2014 Grow Bit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.turbogwt.core.js.collections;
 
 import com.google.gwt.core.client.JsArrayString;
@@ -6,6 +23,13 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A Map from String to Object implementation over a Javascript Object.
+ *
+ * @param <T> The type of the map values
+ *
+ * @author Danilo Reinert
+ */
 public class JsMapWrapper<T> implements Map<String, T> {
 
     // TODO: IMPLEMENT UNIT TESTS
@@ -24,7 +48,7 @@ public class JsMapWrapper<T> implements Map<String, T> {
     @Override
     public boolean containsKey(Object o) {
         checkNotNull(o);
-        assert (o instanceof String) : "Key should be of type String";
+        assert o instanceof String : "Key should be of type String";
 
         String key = (String) o;
         return innerMap.contains(key);
@@ -33,7 +57,7 @@ public class JsMapWrapper<T> implements Map<String, T> {
     @Override
     public boolean containsValue(Object o) {
         checkNotNull(o);
-        assert (o instanceof String) : "Key should be of type String";
+        assert o instanceof String : "Key should be of type String";
 
         JsArrayString keys = innerMap.keys();
         for (int i = 0; i < keys.length(); i++) {
@@ -47,7 +71,7 @@ public class JsMapWrapper<T> implements Map<String, T> {
     @Override
     public T get(Object o) {
         checkNotNull(o);
-        assert (o instanceof String) : "Key should be of type String";
+        assert o instanceof String : "Key should be of type String";
 
         String key = (String) o;
         return innerMap.get(key);
@@ -66,7 +90,7 @@ public class JsMapWrapper<T> implements Map<String, T> {
     @Override
     public T remove(Object o) {
         checkNotNull(o);
-        assert (o instanceof String) : "Key should be of type String";
+        assert o instanceof String : "Key should be of type String";
 
         final String key = (String) o;
         T t = innerMap.get(key);
@@ -89,23 +113,49 @@ public class JsMapWrapper<T> implements Map<String, T> {
 
     @Override
     public Set<String> keySet() {
-        // TODO: IMPLEMENT THIS!
-        return null;
+        // TODO: implement a new set extending this one, reflecting all operations to the map
+        return new JsSafeSet<>(innerMap.keys());
     }
 
     @Override
     public Collection<T> values() {
-        // TODO: IMPLEMENT THIS!
-        return null;
+        // TODO: implement a new collection extending this one, reflecting all operations to the map
+        return new JsArrayList<>(innerMap.values());
     }
 
     @Override
     public Set<Entry<String, T>> entrySet() {
-        // TODO: IMPLEMENT THIS!
+        // TODO: implement a new set extending this one, reflecting all operations to the map
         return null;
     }
 
     private void checkNotNull(Object o) {
         if (o == null) throw new NullPointerException("This map does not support null values");
+    }
+
+    private static class JsEntry<T> implements Entry<String, T> {
+
+        private final JsMapWrapper<T> map;
+        private final String key;
+
+        private JsEntry(JsMapWrapper<T> map, String key) {
+            this.map = map;
+            this.key = key;
+        }
+
+        @Override
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public T getValue() {
+            return map.get(key);
+        }
+
+        @Override
+        public T setValue(T t) {
+            return map.put(key, t);
+        }
     }
 }
