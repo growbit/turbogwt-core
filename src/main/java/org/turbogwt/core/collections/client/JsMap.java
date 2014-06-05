2b8556614dbb0e0d49cbed9f9130dfd673255ea0
Danilo Reinert <daniloreinert@gmail.com>
2014-06-04 23:08:57 -0300

    BREAKING CHANGE: Refactored packages

---
+++ src/main/java/org/turbogwt/core/collections/client/JsMap.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2009 Thomas Broyer
+ * Copyright 2014 Grow Bit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.turbogwt.core.collections.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArrayString;
+
+/**
+ * Map of String to Object implemented on a JavaScriptObject.
+ *
+ * @param <T> Type of mapped values
+ *
+ * @author Thomas Broyer
+ * @author Danilo Reinert
+ */
+public class JsMap<T> extends JavaScriptObject {
+
+    protected JsMap() {
+    }
+
+    public static native <T> JsMap<T> create() /*-{
+        var o = {};
+        Object.defineProperties(o, {__props__: {enumerable: false, writable: true, value: []}});
+        return o;
+    }-*/;
+
+    public final native void clear() /*-{
+        // Although not fast, it's safer than creating new objects
+        for (var key in this) delete this[key];
+        Object.defineProperties(this, {__props__: {enumerable: false, writable: true, value: []}});
+    }-*/;
+
+    public final native T get(String key) /*-{
+        return this[key];
+    }-*/;
+
+    public final native T get(String key, T defaultValue) /*-{
+        return this[key] || defaultValue;
+    }-*/;
+
+    public final void set(String key, T value) {
+        checkNotNull(key);
+        checkNotNull(value);
+        set0(key, value);
+    }
+
+    public final native boolean contains(String key) /*-{
+        return (key in this);
+    }-*/;
+
+    public final native void remove(String key) /*-{
+        if (!this.__props__)
+            Object.defineProperties(this, {__props__: {enumerable: false, writable: true, value: Object.keys(this)}});
+        var i = this.__props__.indexOf(key);
+        if (~i) {
+            this.__props__.splice(i, 1);
+            delete this[key];
+        }
+    }-*/;
+
+    public final native int size() /*-{
+        if (!this.__props__)
+            Object.defineProperties(this, {__props__: {enumerable: false, writable: true, value: Object.keys(this)}});
+        return this.__props__.length;
+    }-*/;
+
+    public final String keyOf(T t) {
+        checkNotNull(t);
+        return keyOf0(t);
+    }
+
+    public final native JsArrayString keys() /*-{
+        return this.__props__;
+    }-*/;
+
+    public final native JsArray<T> values() /*-{
+        var values = [];
+        for (var key in this) values.push(this[key]);
+        return values;
+    }-*/;
+
+    private void checkNotNull(Object o) {
+        if (o == null) throw new NullPointerException("This map does not accept null keys or values.");
+    }
+
+    private native String keyOf0(T t) /*-{
+        for (var key in this)
+            if (t.@java.lang.Object::equals(Ljava/lang/Object;)(this[key])) return key;
+        return null;
+    }-*/;
+
+    private native void set0(String key, T value) /*-{
+        if (!this[key]) {
+            if (!this.__props__)
+                Object.defineProperties(this, {__props__: {enumerable: false, writable: true,
+                    value: Object.keys(this)}});
+            this.__props__.push(key);
+        }
+        this[key] = value;
+    }-*/;
+}
---
+++ src/main/java/org/turbogwt/core/util/client/Native.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Grow Bit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.turbogwt.core.util.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+import javax.annotation.Nullable;
+
+/**
+ * Utility methods derived from browser's native JS.
+ *
+ * @author Danilo Reinert
+ */
+public final class Native {
+
+    private Native() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends JavaScriptObject> T eval(String text) {
+        return (T) evalNative(text);
+    }
+
+    public static native boolean isNumeric(@Nullable String text) /*-{
+        if (text) {
+            return !isNaN(text);
+        }
+        return false;
+    }-*/;
+
+    public static native void log(JavaScriptObject jso) /*-{
+        $wnd.console.log(jso);
+    }-*/;
+
+    public static native void log(String text) /*-{
+        $wnd.console.log(text);
+    }-*/;
+
+    public static native String toFixed(double number) /*-{
+        return number.toFixed();
+    }-*/;
+
+    public static native String toFixed(double number, int fractionalDigits) /*-{
+        return number.toFixed(fractionalDigits);
+    }-*/;
+
+    private static native JavaScriptObject evalNative(String text) /*-{
+        return eval(text);
+    }-*/;
+}
