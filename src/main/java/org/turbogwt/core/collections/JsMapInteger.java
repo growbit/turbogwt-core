0c59c43f1f91cd088722eefc9e039ec9d2490600
Danilo Reinert <daniloreinert@gmail.com>
2014-11-09 14:23:16 -0300

    Header fixing.

--- src/main/java/org/turbogwt/core/collections/JsMapInteger.java
+++ src/main/java/org/turbogwt/core/collections/JsMapInteger.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.turbogwt.core.collections;
 
 import com.google.gwt.core.client.JavaScriptObject;
